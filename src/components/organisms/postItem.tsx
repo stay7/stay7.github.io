4d1bb49cf9c5c2fdb758651a6d143f659d91d37d
stay7 <codeLatte20@gmail.com>
2020-12-13 16:45:55 +0900

    Add mouse over

--- src/components/organisms/postItem.tsx
+++ src/components/organisms/postItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState, CSSProperties } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import PostModel from "../../model/postModel";
 import Link from "../atom/link";
 import Color from "../../constants/color";
@@ -23,36 +23,51 @@ const PostItem = ({ post }: Props) => {
       style={thumbnail && { paddingLeft: 10 }}
     >
       <Link to={path} style={thumbnail && imageStyle}>
-        {thumbnail && <ImgBox src={thumbnail} />}
-        <ContentBox>
-          <Title>{title}</Title>
-          <SubtitleBox>
-            {subtitle && <Subtitle>{subtitle}</Subtitle>}
-          </SubtitleBox>
-          <MetaContainer>
-            <DateText>{date}</DateText>
-            <ReadMore>{over && <span>Read More</span>}</ReadMore>
-          </MetaContainer>
-        </ContentBox>
+        <BorderBox color={over ? Color.blue : Color.white}>
+          {thumbnail && <ImgBox src={thumbnail} />}
+          <ContentBox>
+            <Title>{title}</Title>
+            <SubtitleBox>
+              {subtitle && <Subtitle>{subtitle}</Subtitle>}
+            </SubtitleBox>
+            <MetaContainer>
+              <DateText>{date}</DateText>
+              <ReadMore>{over && <span>Read More</span>}</ReadMore>
+            </MetaContainer>
+          </ContentBox>
+        </BorderBox>
       </Link>
     </Container>
   );
 };
 
 const imageStyle: CSSProperties = {
+  display: "flex",
   width: "100%",
   height: "100%",
-  display: "flex",
   flexDirection: "row",
   alignItems: "center",
 };
 
 const Container = styled.div`
-  height: 150px;
+  display: flex;
+  height: 170px;
   border: 1px solid;
-  padding: 10px 20px 10px 20px;
+  padding: 10px;
   margin: 10px 0px 10px 0px;
   background-color: ${Color.white};
+`;
+
+const BorderBox = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  padding: 10px;
+  ${(props) =>
+    props.color &&
+    css`
+      border: 1px solid ${props.color};
+    `}
 `;
 const ImgBox = styled.img`
   width: 130px;
@@ -60,14 +75,15 @@ const ImgBox = styled.img`
   margin-right: 10px;
 `;
 const ContentBox = styled.div`
-  height: 100%;
   display: flex;
   flex-direction: column;
+  width: 100%;
   justify-content: space-between;
 `;
 const Title = styled.h2`
   padding: 0;
   margin-bottom: 0;
+  border-bottom-width: 0;
 `;
 const SubtitleBox = styled.div`
   flex-grow: auto;
@@ -85,6 +101,7 @@ const MetaContainer = styled.div``;
 const ReadMore = styled.div`
   height: 30px;
   float: right;
+  color: ${Color.blue};
 `;
 
 export default PostItem;
--- src/constants/color.ts
+++ src/constants/color.ts
@@ -2,6 +2,7 @@ const Color = {
   white: "#ffffff",
   black: "#000000",
   gray75: "#757575",
+  blue: "#1167b1",
 };
 
 export default Color;
