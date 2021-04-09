fa4edfd2dbc81c358f443a42c86242927d66b8af
stay7 <codeLatte20@gmail.com>
2021-04-09 21:55:39 +0900

    Delete svg plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,15 +63,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /images/, // See below to configure properly
-        },
-      },
-    },
-
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
