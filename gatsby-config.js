c56f190b0102185e4a26547a74f7345ddd1b214b
stay7 <codelatte20@gmail.com>
2021-10-05 15:27:44 +0900

    Migrate gatsby from v2 to v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,10 +1,14 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+const Octocat = () => <StaticImage src="../images/octocat.png" alt="octocat" />;
+
 const Me = () => (
   <Layout>
     <SEO title="me" />
+    <Octocat />
     <h1>404 bio not found</h1>
   </Layout>
 );
